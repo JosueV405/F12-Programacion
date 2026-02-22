cd93290ed9915f8e2c98709e0013d81b414a1f43
JosueV405 <josuevargasescobar@gmail.com>
2026-02-22 14:20:44 -0600

    hola mundo con nombre y carnet

--- cpp/ejemplos/hola_mundo.cpp
+++ cpp/ejemplos/hola_mundo.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main() {
   std::cout << "Hola C++ desde code-server\n";
-  std::cout << "<<Nombre>> y <<Carnet>>\n";
+  std::cout << "<<Josue Rafael Vargas Escobar>> y <<202604232>>\n";
   return 0;
 }
